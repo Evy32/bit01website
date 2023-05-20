6c1977eb8e608498f81fba9097c89c293ad93955
Evy32 <evbp080116@gmail.com>
2023-05-19 21:04:00 -0500

    Version 0.0.0.2

---
+++ index.js
@@ -0,0 +1,10 @@
+const user = document.getElementById('user');
+const password = document.getElementById('password');
+const boton = document.getElementById('boton');
+
+boton.addEventListener("click", function(){
+    procesarEvento(boton);
+});
+function procesarEvento(boton){
+    
+}
