1be2720bd184aba698858e524595555bc1f7f74b
OneFatte <camiloandres798mail.com>
2023-05-27 19:11:16 -0500

    Modificacion registro

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const user = document.getElementById('user');
 const password = document.getElementById('password');
 const boton = document.getElementById('boton');
 
-boton.addEventListener("click", function(event){
-   event.preventDefault();
+var linkElement = document.createElement("link");
+linkElement.rel = "stylesheet";
+linkElement.href = "assets/registro.css";
+
+
+
+boton.addEventListener("click", function (event) {
+    event.preventDefault();
     procesarEvento(boton);
 });
-function procesarEvento(boton){
+
+function procesarEvento(boton) {
     console.log(user.value);
     console.log(password.value);
+
+    var newWindow = window.open("", "_blank");
+    newWindow.document.write("<div class='contenedor'> <h2>Registrado con exito:</h2>");
+    newWindow.document.write("<p><strong>Nombre:</strong> " + user.value + "</p>");
+    newWindow.document.write("<p><strong>Contraseña:</strong> " + password.value + "</p> </div>");
+    newWindow.document.title = "Registrado!";
+    newWindow.document.head.appendChild(linkElement);
+
 }
